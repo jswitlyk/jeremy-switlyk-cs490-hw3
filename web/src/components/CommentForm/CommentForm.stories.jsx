ee6338614600e19d202897a472eeceec34b1a42b
Jeremy Switlyk <jeremy.switlyk@protonmail.ch>
2023-09-21 15:33:31 -0400

    Mock CreateComment Mutation

--- web/src/components/CommentForm/CommentForm.stories.jsx
+++ web/src/components/CommentForm/CommentForm.stories.jsx
@@ -1,17 +1,21 @@
-// Pass props to your component by passing an `args` object to your story
-//
-// ```jsx
-// export const Primary = {
-//  args: {
-//    propName: propValue
-//  }
-// }
-// ```
-//
-// See https://storybook.js.org/docs/react/writing-stories/args.
-
 import CommentForm from './CommentForm'
 
-export default { component: CommentForm }
+export const generated = () => {
+  mockGraphQLMutation('CreateCommentMutation', (variables, { ctx }) => {
+    const id = Math.floor(Math.random() * 1000)
+    ctx.delay(1000)
 
-export const Primary = {}
+    return {
+      createComment: {
+        id,
+        name: variables.input.name,
+        body: variables.input.body,
+        createdAt: new Date().toISOString(),
+      },
+    }
+  })
+
+  return <CommentForm />
+}
+
+export default { title: 'Components/CommentForm' }
