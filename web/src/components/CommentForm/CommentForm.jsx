139e59569f9c2b4fe2d958ef55ce147ee7195eeb
Jeremy Switlyk <jeremy.switlyk@protonmail.ch>
2023-09-21 15:07:24 -0400

    Add Comment Form Component

---
+++ web/src/components/CommentForm/CommentForm.jsx
@@ -0,0 +1,10 @@
+const CommentForm = () => {
+  return (
+    <div>
+      <h2>{'CommentForm'}</h2>
+      <p>{'Find me in ./web/src/components/CommentForm/CommentForm.jsx'}</p>
+    </div>
+  )
+}
+
+export default CommentForm
