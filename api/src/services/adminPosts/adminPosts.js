5f2ae8a83314719f2e30d3c5a68a206d4c971c4c
Jeremy Switlyk <jeremy.switlyk@protonmail.ch>
2023-09-22 12:25:13 -0400

    Verify Post Ownership for update/delete

--- api/src/services/adminPosts/adminPosts.js
+++ api/src/services/adminPosts/adminPosts.js
@@ -1,4 +1,14 @@
+import { ForbiddenError } from '@redwoodjs/graphql-server'
+
 import { db } from 'src/lib/db'
+
+const verifyOwnership = async ({ id }) => {
+  if (await adminPost({ id })) {
+    return true
+  } else {
+    throw new ForbiddenError("You don't have access to this post")
+  }
+}
 
 export const adminPosts = () => {
   return db.post.findMany({ where: { userId: context.currentUser.id } })
@@ -16,14 +26,18 @@ export const createPost = ({ input }) => {
   })
 }
 
-export const updatePost = ({ id, input }) => {
+export const updatePost = async ({ id, input }) => {
+  await verifyOwnership({ id })
+
   return db.post.update({
     data: input,
     where: { id },
   })
 }
 
-export const deletePost = ({ id }) => {
+export const deletePost = async ({ id }) => {
+  await verifyOwnership({ id })
+
   return db.post.delete({
     where: { id },
   })
